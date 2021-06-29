bd2e7e5679b35b449a302f3f8499f938bf4e3813
ImAkhil24 <akhilbadoni24@gmail.com>
2021-06-29 13:10:36 +0530

    the web app with basic funcionlaity is ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 import './App.css';
 
 const app = () => {
+  if(!localStorage.getItem('username')) return <LoginForm />
   return (
     <ChatEngine
       height = '100vh'
       projectID = 'aee9bd71-18dc-4a15-b8fb-15b3d16f665e'
-      userName = 'akhil'
-      userSecret = 'akhil123'
+      userName = {localStorage.getItem('username')}
+      userSecret = {localStorage.getItem('password')}
       renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
   );
